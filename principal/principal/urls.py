f569404913a69dd325d20d68f6d5ad843bef467d
JDquintero2001 <jdquintero@unicauca.edu.co>
2026-05-30 18:31:30 -0500

    cuarto comentario

--- principal/principal/urls.py
+++ principal/principal/urls.py
@@ -6,7 +6,9 @@ from drf_yasg import openapi
 from django.urls import path, include
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
-
+from equipos.api.router import router_equipo
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 # Serializers define the API representation.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -21,15 +23,16 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
 
 
-# Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r"users", UserViewSet)
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
+
 urlpatterns = [
     path("", include(router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
+    path('api/',     include(router_equipo.urls)),
+    path('api/',     include(router_equipo.urls)),
 ]
 
 schema_view = get_schema_view(
@@ -45,8 +48,4 @@ schema_view = get_schema_view(
    permission_classes=(permissions.AllowAny,),
 )
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("api-auth/", include("rest_framework.urls")),
-    path('api/',     include(router_equipo.urls)),
-]
+
