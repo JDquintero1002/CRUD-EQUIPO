c58acfe61eac15a9e6658bc370aded34fc9d2e24
JDquintero2001 <jdquintero@unicauca.edu.co>
2026-05-30 20:16:49 -0500

    terminado

--- principal/equipos/api/views.py
+++ principal/equipos/api/views.py
@@ -1,10 +1,11 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import permissions
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from equipos.models import Equipo
 from equipos.api.serializers import EquipoSerializer
 
 
 class EquipoViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.AllowAny]
     serializer_class   = EquipoSerializer
     queryset           = Equipo.objects.all()
